1800563b9761b7f7e4af7da3c05ffd6e0a2fdfc3
gvliegen <gvliegen@oa-gvliegen.eu>
2025-11-17 00:30:23 +0100

    feat: plotting of loss through training, weights for pairs

--- xgboost_functions.py
+++ xgboost_functions.py
@@ -1,5 +1,4 @@
 import numpy as np
-from pygam import s, LinearGAM
 import matplotlib.pyplot as plt
 import math
 import pprint
@@ -7,20 +6,22 @@ import polars as pl
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import sympy as sp
-from typing import List, Dict, Tuple, Callable, Any, Optional
-import mlflow
-import mlflow.pyfunc
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
+from typing import Tuple
 
 import random
 import numpy as np
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score
 from xgboost.callback import EvaluationMonitor
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    confusion_matrix,
+    classification_report
+)
 
 class RaceModel:
     """
@@ -46,14 +47,6 @@ class RaceModel:
         # 0-4: name ┆ year ┆ score ┆ rank ┆ Onedayraces ┆ 
         # 5-9: GC ┆ TT ┆ Sprint ┆ Climber ┆ Hills
 
-        self.model = xgb.XGBClassifier(
-            n_estimators=100,
-            max_depth=4,
-            learning_rate=0.05,
-            early_stopping_rounds=100,
-            eval_metric="logloss",
-            callbacks=[EvaluationMonitor(show_stdv=True)]
-        )
 
         self.rider_feature_idxs = [2, 3, 4, 5, 6, 7, 8, 9]
         self.rider_features = [
@@ -75,54 +68,88 @@ class RaceModel:
             "classification_encoded",
             "year",
             "startlist_score", #Check present in pairs
-        ]
+        ]       
 
-        self.model = xgb.XGBClassifier(
-            n_estimators=100,
-            max_depth=4,
-            learning_rate=0.1,
-            use_label_encoder=False,
-            eval_metric="logloss"
-        )           
 
-    def fit(self, X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray, y_test: np.ndarray) -> None:
-        params = {
-            "objective": "binary:logistic",
-            "eval_metric": "logloss"
-        }
-        dtrain = xgb.DMatrix(X_train, label=y_train)
-        dtest = xgb.DMatrix(X_test, label=y_test)
+    def fit(self, X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray, y_test: np.ndarray, train_weights: np.ndarray, test_weights: np.ndarray) -> None:
 
+
+        dtrain = xgb.DMatrix(X_train, label=y_train, weight=train_weights)
+        dtest = xgb.DMatrix(X_test, label=y_test, weight=test_weights)
+
+
+        epochs = []
+        test_errors = []
+        train_errors = []
+        class PrintIterationCallback(xgb.callback.TrainingCallback):
+            def after_iteration(xgb_self, model, epoch, evals_log):
+                # if epoch % 1 == 0:
+                test_y_pred_proba = model.predict(dtest)
+                train_y_pred_proba = model.predict(dtrain)
+
+                test_average_error = mean_absolute_error(y_test, test_y_pred_proba)
+                train_average_error = mean_absolute_error(y_train, train_y_pred_proba)
+                epochs.append(epoch)
+                test_errors.append(test_average_error)
+                train_errors.append(train_average_error)
+                # print(f"Iteration {epoch}, F1 Score: {f1:.4f}")
+
+                # Return False to continue training, True to stop
+                return False
+            
         bst = xgb.train(
-            params,
-            dtrain,
-            num_boost_round=100,
-            evals=[(dtest, "test")],
-            # early_stopping_rounds=50,
-            verbose_eval=True
+            dtrain = dtrain,
+            num_boost_round=200,
+            evals=[],
+            params = {
+                "eval_metric": "logloss",
+                "min_child_weight": 1,
+                "max_depth": 6,
+                "subsample": 0.5,
+            },
+            callbacks=[PrintIterationCallback()],
         )
         self.bst = bst
+
+        self.print_training_progress(epochs = epochs, test_errors = test_errors, train_errors = train_errors)
+        return
+
+    def print_training_progress(self, epochs, test_errors, train_errors):
+        plt.figure(figsize=(10, 6))
+        plt.plot(epochs, test_errors, label='Test Error', color='blue')
+        plt.plot(epochs, train_errors, label='Train Error', color='orange')
+        plt.xlabel('Epochs')
+        plt.ylabel('Mean Absolute Error')
+        plt.title('Training Progress')
+        plt.legend()
+        plt.grid()
+        plt.show()
         return
     
     def to_xgboost_format(self, riders_data: pl.DataFrame, race_data: pl.DataFrame) -> np.ndarray:
         X = []
         y = []
+        X_weights = []
         all_race_ids: list[int] = race_data.select(
             ["race_id"]
         ).unique().to_series().to_list()
-        nr_races = 100 #len(all_race_ids) #100
+        nr_races = len(all_race_ids) #100
         nr_pairs_per_race = 100
 
         #TODO: VECTORIZE THIS
         for race_id in all_race_ids[:nr_races]:
-            rider_pairs = self.get_rider_pairs(
-                race_id=race_id,
-                race_data=race_data
-            )
-            rider_pairs = rider_pairs[:min(nr_pairs_per_race, len(rider_pairs))]
             race_year = race_data.filter(
                 pl.col("race_id") == race_id
             ).select("year").to_numpy()[0][0]
+
+            if race_year < 2018:
+                continue
+
+            rider_pairs, pair_weights = self.get_rider_pairs(
+                race_id=race_id,
+                race_data=race_data
+            )
+            rider_pairs = rider_pairs#[:min(nr_pairs_per_race, len(rider_pairs))]
 
             race_Y = np.random.choice([0, 1], size=(len(rider_pairs),))
 
@@ -130,7 +157,7 @@ class RaceModel:
                 rider_pairs=rider_pairs,
                 riders_data=riders_data,
                 race_year=race_year,
-                # race_Y=race_Y
+                race_Y=race_Y
             )
 
             race_features = race_data.filter(
@@ -140,10 +167,12 @@ class RaceModel:
 
             race_X = np.hstack([rider_pair_features, race_features])
             X.append(race_X)
+            X_weights.append(pair_weights)
             y.append(race_Y)
 
-        y = np.vstack(y).flatten()
+        y = np.concatenate(y)
         X = np.vstack(X)
+        X_weights = np.vstack(X_weights)
         print(f"training on {len(y)} pairs")
 
         return X, y
@@ -153,10 +182,10 @@ class RaceModel:
         rider_pairs: list[tuple[str, str]],
         riders_data: pl.DataFrame,
         race_year: int,
-        # race_Y: np.ndarray
+        race_Y: np.ndarray
     ) -> np.ndarray:    
         """
-        if race_Y == 0, shift the pair features since the first rider in data was the first in the race
+        TODO: if race_Y == 0, shift the pair since the first rider in data was the first in the race
         """
         pair_features = []
         years_to_go_back = 3
@@ -164,29 +193,63 @@ class RaceModel:
             str(race_year - i) for i in range(1, years_to_go_back + 1)
         ]
 
-        for rider_0_name, rider_1_name in rider_pairs: #TODO: make more efficient
-            rider_0_data = riders_data.filter(
-                pl.col("name") == rider_0_name
-            ).filter(
-                pl.col("year").is_in(years_to_go_back)
-            ).select(self.rider_features).to_numpy().flatten().astype(np.float32)
+        rider_0_filters = pl.DataFrame(
+            {
+                "name_0": rider_pair_names[0],
+                "name_1": rider_pair_names[1],
+                "year": year
+            } if y == 1 else {
+                "name_0": rider_pair_names[1],
+                "name_1": rider_pair_names[0],
+                "year": year
+            }
+            for rider_pair_names, y in zip(rider_pairs, race_Y)
+            for year in years_to_go_back
+        )
 
-            rider_1_data = riders_data.filter(
-                pl.col("name") == rider_1_name
-            ).filter(
-                pl.col("year").is_in(years_to_go_back)
-            ).select(self.rider_features).to_numpy().flatten().astype(np.float32)
+        rider_0_data: pl.DataFrame = riders_data.join(
+            rider_0_filters,
+            left_on=["name", "year"],
+            right_on=["name_0", "year"],
+            how="right" #preserve order of pairs
+        ).fill_null(
+            np.NAN
+        ).select(self.rider_features)
+        rider_0_data: np.array = rider_0_data.to_numpy().astype(np.float32).reshape(len(rider_pairs), 3 * len(self.rider_features))
 
-            target_len = len(years_to_go_back) * len(self.rider_features)
+        rider_1_data: pl.DataFrame = riders_data.join(
+            rider_0_filters,
+            left_on=["name", "year"],
+            right_on=["name_1", "year"],
+            how="right" #preserve order of pairs
+        ).fill_null(
+            np.NAN
+        ).select(self.rider_features)
+        rider_1_data: np.array = rider_1_data.to_numpy().astype(np.float32).reshape(len(rider_pairs), 3 * len(self.rider_features))
 
-            rider_0_data = np.pad(rider_0_data, (0, target_len - len(rider_0_data)), mode='constant', constant_values=np.NAN)
-            rider_1_data = np.pad(rider_1_data, (0, target_len - len(rider_1_data)), mode='constant', constant_values=np.NAN)
-            features = np.concatenate([rider_0_data, rider_1_data])
-            pair_features.append(features)
+            # for rider_0_name, rider_1_name in rider_pairs: #TODO: make more efficient
+            #     rider_0_data = riders_data.filter(
+            #         pl.col("name") == rider_0_name
+            #     ).filter(
+            #         pl.col("year").is_in(years_to_go_back)
+            #     ).select(self.rider_features).to_numpy().flatten().astype(np.float32)
 
-        pair_features = np.array(pair_features)
+            #     rider_1_data = riders_data.filter(
+            #         pl.col("name") == rider_1_name
+            #     ).filter(
+            #         pl.col("year").is_in(years_to_go_back)
+            #     ).select(self.rider_features).to_numpy().flatten().astype(np.float32)
+
+            #     target_len = len(years_to_go_back) * len(self.rider_features)
+
+            #     rider_0_data = np.pad(rider_0_data, (0, target_len - len(rider_0_data)), mode='constant', constant_values=np.NAN)
+            #     rider_1_data = np.pad(rider_1_data, (0, target_len - len(rider_1_data)), mode='constant', constant_values=np.NAN)
+            #     features = np.concatenate([rider_0_data, rider_1_data])
+            #     pair_features.append(features)
+
+            # pair_features = np.array(pair_features)
         # year_cols = [i * len(self.rider_features) for i in range(len(years_to_go_back) * 2)]
-        feature_cols = pair_features[:, 0:len(rider_0_data)] - pair_features[:, len(rider_0_data):] 
+        feature_cols = rider_0_data - rider_1_data
 
 
         #shift pair depending on y value
@@ -199,29 +262,39 @@ class RaceModel:
 
         return feature_cols
 
-    def get_rider_pairs(self, race_id: int, race_data: pl.DataFrame) -> list[tuple[str, str]]:
+    def get_rider_pairs(self, race_id: int, race_data: pl.DataFrame, min_top_rank: int = 25) -> list[tuple[str, str, float]]:
         """
-        pairs of rider names, rider 0 is the favorite
+        returns pairs of rider names and weight of importance, rider 0 is the favorite
 
         selects pairs if first rider is in top 25
 
-        returns names only
+        returns 
+            - names in the pair
+            - weights: If a pair has weight w, then in the loss it behaves like it appears w times.
+                - weight of highest rank rider
+                - rank 1: w = 2, rank 25: w = 1
         """
+        if min_top_rank is None:
+            nr_riders = race_data.filter(pl.col("race_id") == race_id).height
+            min_top_rank = nr_riders
+
         top_riders = race_data.filter(
             pl.col("race_id") == race_id
-        ).sort("rank").head(25)
+        ).sort("rank").head(min_top_rank)
         all_riders = race_data.filter(
             pl.col("race_id") == race_id
         ).sort("rank")            
         pairs = []
+        weights = []
         for i, top_rider in enumerate(top_riders["name"]):
             for j, other_rider in enumerate(all_riders["name"]):
                 if i < j:
                     pairs.append((top_rider, other_rider))
-        return pairs
+                    weights.append(1 + (min_top_rank - (i + 1)) / min_top_rank)  #weight between 1 and 2
+        return pairs, weights
 
 
-    def split_train_test(self, X: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    def split_train_test(self, X: np.ndarray, y: np.ndarray, weights: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """
         splits data based on year, year is second to last column
 
@@ -237,59 +310,120 @@ class RaceModel:
 
         X_train = X[train_mask][:, :-2]
         y_train = y[train_mask]
+        train_weights = weights[train_mask]
 
         X_test = X[test_mask][:, :-2]
         y_test = y[test_mask]
+        test_weights = weights[test_mask]
 
-        return X_train, y_train, X_test, y_test
+        return X_train, y_train, train_weights, X_test, y_test, test_weights
 
-    def evaluate_model(self, X_test: np.ndarray, y_test: np.ndarray) -> None:
-        dtest = xgb.DMatrix(X_test, label=y_test)
-        y_pred = self.bst.predict(dtest)
-        accuracy = np.mean(y_pred == y_test)
-        auc = roc_auc_score(y_test, self.bst.predict(dtest))
-        print(f"Test Accuracy: {accuracy:.4f}")
-        print(f"Test AUC: {auc:.4f}")
+    def evaluate_model(self, X_test: np.ndarray, y_test: np.ndarray, test_weights: np.ndarray) -> None:
+        dtest = xgb.DMatrix(X_test, label=y_test, weight=test_weights)
+        y_pred_proba = self.bst.predict(dtest)
+        # accuracy = np.mean(y_pred == y_test)
+        # auc = roc_auc_score(y_test, self.bst.predict(dtest))
+        # print(f"Test Accuracy: {accuracy:.4f}")
+        # print(f"Test AUC: {auc:.4f}")
 
 
-    # y_pred = (y_pred_proba > 0.5).astype(int)
+        y_pred = (y_pred_proba > 0.5).astype(int)
 
-    # # Compute metrics
-    # accuracy = accuracy_score(y_test, y_pred)
-    # precision = precision_score(y_test, y_pred, zero_division=0)
-    # recall = recall_score(y_test, y_pred, zero_division=0)
-    # f1 = f1_score(y_test, y_pred, zero_division=0)
-    # auc = roc_auc_score(y_test, y_pred_proba)
-    # cm = confusion_matrix(y_test, y_pred)
+        # Compute metrics
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred, zero_division=0)
+        recall = recall_score(y_test, y_pred, zero_division=0)
+        f1 = f1_score(y_test, y_pred, zero_division=0)
+        auc = roc_auc_score(y_test, y_pred_proba)
+        cm = confusion_matrix(y_test, y_pred)
 
-    # # Print results
-    # print("=== Evaluation Metrics ===")
-    # print(f"Accuracy:  {accuracy:.4f}")
-    # print(f"Precision: {precision:.4f}")
-    # print(f"Recall:    {recall:.4f}")
-    # print(f"F1 Score:  {f1:.4f}")
-    # print(f"AUC:       {auc:.4f}")
-    # print("Confusion Matrix:")
-    # print(cm)
-    # print("\nDetailed classification report:")
-    # print(classification_report(y_test, y_pred, digits=4))
+        # Print results
+        print("=== Evaluation Metrics ===")
+        print(f"Accuracy:  {accuracy:.4f}")
+        print(f"Precision: {precision:.4f}")
+        print(f"Recall:    {recall:.4f}")
+        print(f"F1 Score:  {f1:.4f}")
+        print(f"AUC:       {auc:.4f}")
+        print("Confusion Matrix:")
+        print(cm)
+        print("\nDetailed classification report:")
+        print(classification_report(y_test, y_pred, digits=4))
 
         return
 
-    
     def train_model(self, riders_data: pl.DataFrame, race_data: pl.DataFrame) -> None:
-        X, y = self.to_xgboost_format(riders_data, race_data)
-        X_train, y_train, X_test, y_test = self.split_train_test(X, y)
-        self.fit(X_train, y_train, X_test, y_test)
+        X, y, x_weights = self.to_xgboost_format(riders_data, race_data)
+        X_train, y_train, train_weights, X_test, y_test, test_weights = self.split_train_test(X, y, x_weights)
+        self.fit(X_train, y_train, X_test, y_test, train_weights, test_weights)
         print("Model fitted")
-        self.evaluate_model(X_test, y_test)
+        self.evaluate_model(X_test, y_test, test_weights=test_weights)
         print("Model evaluated")
+
+    def predict_race(self, riders_data: pl.DataFrame, race_data: pl.DataFrame, race_id: str):
+        rider_pairs = self.get_rider_pairs(
+            race_id = race_id,
+            race_data = race_data,
+            min_top_rank = None
+        )
+        #TODO: add some random switching of pairs 
+        race_year = race_data.filter(
+            pl.col("race_id") == race_id
+        ).select("year").to_numpy()[0][0]
+        race_Y = np.ones(len(rider_pairs))  #dummy
+        rider_features = self.get_rider_pair_features(
+            rider_pairs = rider_pairs,
+            riders_data = riders_data,
+            race_year = race_year,
+            race_Y = race_Y
+            # rider_pairs: list[tuple[str, str]],
+            # riders_data: pl.DataFrame,
+            # race_year: int,
+            # race_Y: np.ndarray
+        )
+        dtest = xgb.DMatrix(rider_features, label=race_Y)
+        y_pred_proba = self.bst.predict(dtest)
+        # pair_outcomes = self.bst.predict(rider_features)  #probability that rider 0 wins
+        riders = set(
+            [rider for pair in rider_pairs for rider in pair]
+        )
+        rider_scores = {
+            rider: [] for rider in riders
+        }
+        for (first, second), pred_y in zip(rider_pairs, y_pred_proba):
+            rider_scores[first].append( 0 + pred_y)
+            rider_scores[second].append(1 - pred_y)
+        rider_scores = {
+            rider: np.sum(scores) for rider, scores in rider_scores.items()
+        }
+        top10 = sorted(rider_scores.items(), key=lambda x: x[1], reverse=True)[:10]
+
+        # Print nicely
+        for name, score in top10:
+            print(f"{name}: {score:.4f}")
+
+        pass
 
 def main():
     riders_data = pl.read_parquet("data/rider_data.parquet")
     race_data = pl.read_parquet("data/features_df.parquet")
     model = RaceModel()
     model.train_model(riders_data, race_data)
+
+    # races = pl.read_parquet("data/races_df.parquet")
+    # print(races.filter(pl.col("date") == "2024-07-20").head(5))
+    rvv_id = "R487e2808"
+    tdf_2024_20_id = "R5e8c7e1a"
+    model.predict_race(
+        riders_data=riders_data,
+        race_data=race_data,
+        race_id=rvv_id
+    )
+
+    model.predict_race(
+        riders_data=riders_data,
+        race_data=race_data,
+        race_id=tdf_2024_20_id
+    )
 
 if __name__ == "__main__":
     main()
